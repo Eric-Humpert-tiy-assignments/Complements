b4a530480da3031c83a1c719f7d6b866dc656ee8
Eric Humpert <EricMac@ricks-mbp.apollosrc.root>
2016-04-06 15:58:22 -0500

    added the iteration checking for the application

--- complements.js
+++ complements.js
@@ -2,13 +2,24 @@
 // which returns true if any two numbers in
 // the array sum to the number.
 
-function complements(){}
+function complements(arr, number){
+  for (var val of arr) {
+    for (var val2 of arr) {
+      if (val + val2 === number){
+        return true;
+      }      
+    }
+  }
+  return false;
+}
+
+
 
 var odds = [1, 3, 5, 7, 9, 11]
 var ints = [-11, 40, 17, -5, -1, -11, 2, 9]
 
-console.assert( complements(odds, 4) === true )
-console.assert( complements(odds, 1) === false )
-console.assert( complements(ints, -22) === true )
-console.assert( complements(ints, 16) === true )
-console.assert( complements(ints, 40) === false )
+console.assert( complements(odds, 4) === true, "first" )
+console.assert( complements(odds, 1) === false, "second" )
+console.assert( complements(ints, -22) === true, "third" )
+console.assert( complements(ints, 16) === true, "fourth" )
+console.assert( complements(ints, 40) === false, "fifth" )
